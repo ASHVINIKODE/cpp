5c1be871c417b61b7c5a86421a32890a7d5e623d
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-08-30 22:27:37 +0530

    initial comment

---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    float average;
+    char gender;
+    bool isit;
+    age = 21;
+    average = 7.8;
+    gender = 'F';
+    isit = true;
+
+    cout << age << endl
+         << average << endl
+         << gender << endl
+         << isit << endl;
+    cout << "This is printed using Cout" << endl
+         << "Cout is awesome";
+    return 0;
+}
