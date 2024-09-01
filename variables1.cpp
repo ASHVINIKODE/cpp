ca45f5d362680176f75d2c357ebfc90878ee75fc
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-09-01 23:52:03 +0530

    initial commit

---
+++ variables1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    float avg;
+    cout << "Enter your age and average" << endl;
+    cin >> age >> avg;
+    cout << "your age is " << age << endl
+         << "average is " << avg << endl;
+}
