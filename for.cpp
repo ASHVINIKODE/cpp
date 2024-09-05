43bee605f976d046229807162515d3bd339d143b
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-09-05 22:12:39 +0530

    initial commit

---
+++ for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 1; i <= 30; i++)
+    {
+        cout << i << "-->coding is love..." << endl;
+    }
+    return 0;
+}
