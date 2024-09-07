5bb5be7ff2891929c21a3c82634cd9cdac6469b1
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-09-07 23:53:20 +0530

    initial commit

---
+++ functions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// void display()
+// {
+//     cout << "welcome to functions" << endl;
+// };
+// void place()
+// {
+//     cout << "welcome to programming" << endl;
+// };
+void display();
+void place();
+int main()
+{
+    display();
+    place();
+    display();
+    display();
+    return 0;
+}
+void display()
+{
+    cout << "welcome to functions" << endl;
+};
+void place()
+{
+    cout << "welcome to programming" << endl;
+};
