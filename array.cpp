c52ee73cfca75dff4b1d02a105a7b2e53b31c1fd
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-09-07 23:39:34 +0530

    initial commit

--- array.cpp
+++ array.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-    int marks[6] = {45, 56, 76, 55, 67, 88};
-    cout << marks[2] << endl;
-    return 0;
-    // int marks[2][6] = {{40, 66, 67, 43, 47, 87}, {23, 54, 65, 76, 22, 65}};
-    // cout << marks[4][3];
+    // int marks[6] = {45, 56, 76, 55, 67, 88};
+    // cout << marks[2] << endl;
     // return 0;
+    int marks[2][6] = {{40, 66, 67, 43, 47, 87}, {23, 54, 65, 76, 22, 65}};
+    cout << marks[1][3];
+    return 0; 
 }
