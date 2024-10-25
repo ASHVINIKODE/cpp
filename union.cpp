73e2183a8e03fcfe7c85f5dd0033e0c5f40537fe
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-10-25 23:36:19 +0530

    initial commit

---
+++ union.cpp
@@ -0,0 +1,17 @@
+// user defined data type similar to structures but in union we can only store the value in onlu one member at a time thats because the memory is shared between the members of the union unlike the structures where each structure members has allocated its own memory but in unions the memory is shared between all the union members.
+#include <iostream>
+using namespace std;
+union emp
+{
+    int id;
+    float sal;
+} anil, rashmit;
+int main()
+{
+    emp anjali, ayaan;
+    anjali.id = 25;
+    anjali.sal = 30000;
+    cout << anjali.id << endl;
+    cout << anjali.sal << endl;
+    return 0;
+}
