36fe8e1afc0200952c3f36a5bfee9c15c7a3499a
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-10-25 23:35:59 +0530

    initial commit

---
+++ sizeof.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+struct student
+{
+    int rollno;
+    char sex;
+    string name;
+};
+int main()
+{
+    int age;
+    cout << "int-->" << sizeof(int) << endl;
+    cout << "short int-->" << sizeof(short int) << endl;
+    cout << "char-->" << sizeof(char) << endl;
+    cout << "float-->" << sizeof(float) << endl;
+    cout << "double-->" << sizeof(double) << endl;
+    cout << "bool-->" << sizeof(bool) << endl;
+    cout << "string-->" << sizeof(string) << endl;
+    cout << "age-->" << sizeof(age) << endl;
+    cout << "struct student-->" << sizeof(struct student) << endl;
+    cout << "rollno-->" << sizeof(int) << endl;
+
+    return 0;
+}
