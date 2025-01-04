77702349d3601667c64f5a784ac0418267d38fff
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-01-04 10:34:34 +0530

    initial commit

---
+++ class_and_object.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class HumanBeing
+{
+public:
+    void display()
+    {
+        cout << "Hello I am a human being" << endl;
+    }
+};
+int main()
+{
+    HumanBeing anil;
+    anil.display();
+    return 0;
+}
